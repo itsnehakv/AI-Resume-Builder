1e9aa577491f38eb8e6f07368fb872bbe84783e4
itsnehakv <itsnehakv@gmail.com>
2026-01-16 12:46:25 +0530

    Implement resume loading functionality in ResumeBuilder component

--- client-side/src/pages/ResumeBuilder.jsx
+++ client-side/src/pages/ResumeBuilder.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { dummyResumeData } from "../assets/assets";
+import { Link, useParams } from "react-router-dom";
+import { ArrowLeftIcon } from "lucide-react";
 
 const ResumeBuilder = () => {
-  return <div>ResumeBuilder</div>;
+  const { resumeId } = useParams();
+
+  const [resumeData, setResumeData] = React.useState({
+    _id: "",
+    title: "",
+    personal_info: {},
+    professional_summary: "",
+    experience: [],
+    education: [],
+    project: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#5EEAD4",
+    public: false,
+  });
+
+  const loadExistingResume = async () => {
+    const resume = dummyResumeData.find((resume) => resume._id === resumeId);
+    if (resume) {
+      setResumeData(resume);
+      document.title = resume.title;
+    }
+  };
+
+  useEffect(() => {
+    loadExistingResume;
+  }, []);
+
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto px-4 py-6">
+        <Link
+          to={"/app"}
+          className="inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all"
+        >
+          <ArrowLeftIcon className="size-4" /> Back To DashBoard
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ResumeBuilder;
