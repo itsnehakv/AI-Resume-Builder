eeeb32e3d0a2895598b5ff670ce787dd561261ab
itsnehakv <itsnehakv@gmail.com>
2026-01-17 02:04:05 +0530

    Add PersonalInfoForm component for user personal information input

---
+++ client-side/src/Components/PersonalInfoForm.jsx
@@ -0,0 +1,150 @@
+import {
+  BriefcaseBusiness,
+  Globe,
+  Linkedin,
+  Mail,
+  MapPin,
+  Phone,
+  User,
+  UserIcon,
+} from "lucide-react";
+import React from "react";
+
+export const PersonalInfoForm = ({
+  data,
+  onChange,
+  removeBackground,
+  setRemoveBackground,
+}) => {
+  const handleChange = (field, value) => {
+    onChange({ ...data, [field]: value });
+  };
+
+  const fields = [
+    {
+      key: "full_name",
+      label: "Full Name",
+      type: "text",
+      icon: User,
+      required: true,
+    },
+    {
+      key: "email",
+      label: "Email Address",
+      type: "email",
+      icon: Mail,
+      required: true,
+    },
+    {
+      key: "phone",
+      label: "Phone Number",
+      type: "telephone",
+      icon: Phone,
+    },
+    {
+      key: "location",
+      label: "Location",
+      type: "text",
+      icon: MapPin,
+    },
+    {
+      key: "profession",
+      label: "Profession",
+      type: "text",
+      icon: BriefcaseBusiness,
+    },
+    {
+      key: "linkedin",
+      label: "LinkedIn Profile",
+      type: "url",
+      icon: Linkedin,
+    },
+    {
+      key: "website",
+      label: "Personal Website",
+      type: "url",
+      icon: Globe,
+    },
+  ];
+  return (
+    <div>
+      <h3 className="text-lg font-semibold text-gray-900">
+        Personal Information
+      </h3>
+      <p className="text-sm text-gray-600">
+        Get started with the personal information!
+      </p>
+      <div className="flex items-center gap-2">
+        <label>
+          {data.image ? (
+            <img
+              src={
+                typeof data.image === "string"
+                  ? data.image
+                  : URL.createObjectURL(data.image)
+              }
+              alt="User Image"
+              className="h-24 w-24 rounded-full object-cover mt-5 ring ring-slate-300
+                hover:opacity-80"
+            />
+          ) : (
+            <div className="inline-flex items-center gap-2 mt-5 text-slate-600 hover:text-slate-700 cursor-pointer">
+              <UserIcon className="size-10 p-2.5 border rounded-full" />
+              Upload User Image
+            </div>
+          )}
+          <input
+            type="file"
+            accept="image/jpeg , image/png"
+            className="hidden"
+            onChange={(e) => handleChange("image", e.target.files[0])}
+          />
+        </label>
+        {data.image && typeof data.image === "object" && (
+          <div className="flex flex-col gap-1 pl-4 text-sm">
+            <p>Remove Background</p>
+            <label
+              className="relative inline-flex items-center cursor-pointer
+                text-gray-900 gap-3"
+            >
+              <input
+                type="checkbox"
+                className="sr-only peer"
+                onChange={() => setRemoveBackground((prev) => !prev)}
+                checked={removeBackground}
+              />
+              <div className="w-9 h-5 bg-slate-300 rounded-full peer peer-checked:bg-green-600 transition-colors duration-200">
+                <span
+                  className="dot absolute left-1 top-1 w-3 h-3 bg-white rounded-full
+                 transition-transform duration-200 ease-in-out peer-checked:translate-x-4"
+                ></span>
+              </div>
+            </label>
+          </div>
+        )}
+      </div>
+
+      {fields.map((field) => {
+        const Icon = field.icon;
+        return (
+          <div key={field.key} className="space-y-1 mt-5">
+            <label className="flex items-center gap-2 text-sm font-medium text-gray-600">
+              <Icon className="size-4" />
+              {field.label}
+              {field.required && <span className="text-red-500">*</span>}
+            </label>
+            <input
+              type={field.type}
+              value={data[field.key] || ""}
+              onChange={(e) => handleChange(field.key, e.target.value)}
+              className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-1g focus:ring 
+              focus:ring-blue-500 focus: border-blue-500 outline-none transition-colors text-sm"
+              placeholder={`Enter your ${field.label.toLowerCase()}`}
+              required={field.required}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
