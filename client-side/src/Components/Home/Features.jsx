1fb7e872b9d114e82a0a102cdadabdce3b779a87
itsnehakv <itsnehakv@gmail.com>
2026-01-17 20:21:32 +0530

    Updated heading and description for features, changed the svgs for each feature, changed color for a feature card hover:border

--- client-side/src/Components/Home/Features.jsx
+++ client-side/src/Components/Home/Features.jsx
@@ -1,91 +1,29 @@
 import React, { useState } from "react";
-import { Zap } from "lucide-react";
+import { Brain, FilePenLine, Settings2, Zap } from "lucide-react";
 import Title from "./Title.jsx";
 
 const Features = () => {
   const features = [
     {
-      title: "Real-Time Analytics",
+      title: "Smart Resume Generation",
       description:
-        "Get instant insights into your finances with live dashboards.",
+        "Get instant insights into your finances with live dashboardsCreate professional, polished resumes in seconds with AI-powered formatting and content suggestions.",
       iconColor: "violet",
-      icon: (
-        <svg
-          width="28"
-          height="28"
-          viewBox="0 0 28 28"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M14 18.667V24.5m4.668-8.167V24.5m4.664-12.833V24.5m2.333-21L15.578 13.587a.584.584 0 0 1-.826 0l-3.84-3.84a.583.583 0 0 0-.825 0L2.332 17.5M4.668 21v3.5m4.664-8.167V24.5"
-            stroke="#7F22FE"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      ),
+      icon: <Brain size={45} className="text-fuchsia-900" />,
     },
     {
-      title: "Bank-Grade Security",
+      title: "Real-Time Editing",
       description:
-        "End-to-end encryption, 2FA, compliance with GDPR standards.",
+        "End-to-end Edit sections, rearrange content, and see instant updates to your resume preview.",
       iconColor: "green",
-      icon: (
-        <svg
-          width="28"
-          height="28"
-          viewBox="0 0 28 28"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M14 11.667A2.333 2.333 0 0 0 11.667 14c0 1.19-.117 2.929-.304 4.667m4.972-3.36c0 2.776 0 7.443-1.167 10.36m5.004-1.144c.14-.7.502-2.683.583-3.523M2.332 14a11.667 11.667 0 0 1 21-7m-21 11.667h.01m23.092 0c.233-2.333.152-6.246 0-7"
-            stroke="#00A63E"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M5.832 22.75C6.415 21 6.999 17.5 6.999 14a7 7 0 0 1 .396-2.333m2.695 13.999c.245-.77.525-1.54.665-2.333m-.255-15.4A7 7 0 0 1 21 14v2.333"
-            stroke="#00A63E"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      ),
+      icon: <FilePenLine size={40} className=" text-teal-800" />,
     },
     {
-      title: "Customizable Reports",
+      title: "Customizable Templates",
       description:
-        "Export professional, audit-ready financial reports for tax or internal review.",
+        "Choose from aSelect from a wide range of sleek, professional templates that are fully optimized for Applicant Tracking Systems (ATS).",
       iconColor: "orange",
-      icon: (
-        <svg
-          width="28"
-          height="28"
-          viewBox="0 0 28 28"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M4.668 25.666h16.333a2.333 2.333 0 0 0 2.334-2.333V8.166L17.5 2.333H7a2.333 2.333 0 0 0-2.333 2.333v4.667"
-            stroke="#F54900"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M16.332 2.333V7a2.334 2.334 0 0 0 2.333 2.333h4.667m-21 8.167h11.667M10.5 21l3.5-3.5-3.5-3.5"
-            stroke="#F54900"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      ),
+      icon: <Settings2 size={40} className="text-purple-800" />,
     },
   ];
 
@@ -122,7 +60,7 @@ const Features = () => {
                       ? "#7F22FE"
                       : feature.iconColor === "green"
                       ? "#00A63E"
-                      : "#F54900"
+                      : "#611acc"
                     : "#e5e7eb",
                 backgroundColor: hoverIndex === index ? "#f0efff" : "#ffffff",
                 boxShadow:
