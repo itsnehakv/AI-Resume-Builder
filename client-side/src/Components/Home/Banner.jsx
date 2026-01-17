61975313091ed00619dac9fdcf969cf3c60aedb4
itsnehakv <itsnehakv@gmail.com>
2026-01-17 17:03:06 +0530

    Changed banner message

--- client-side/src/Components/Home/Banner.jsx
+++ client-side/src/Components/Home/Banner.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="flex flex-wrap items-center justify-between w-full px-4 md:px-14 py-2 font-medium text-sm text-white text-center bg-gradient-to-r from-violet-500 to-purple-100">
-      <p>Build Faster with Responsive Tailwind CSS Templates</p>
+      <p>
+        Build your perfect resume in minutes with AI-powered personalization.
+      </p>
       <a
         href="https://prebuiltui.com"
         className="flex items-center gap-1 px-3 py-1 rounded-lg text-violet-600 bg-violet-50 hover:bg-slate-100 transition active:scale-95 ml-3"
