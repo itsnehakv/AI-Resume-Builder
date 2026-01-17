3b386948680211314a23a6f6d5417f118c66b7d6
itsnehakv <itsnehakv@gmail.com>
2026-01-17 20:23:16 +0530

    Added Layout page for dashboard

--- client-side/src/pages/Layout.jsx
+++ client-side/src/pages/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Navbar from "../Components/Navbar";
 import { Outlet } from "react-router-dom";
+// import Dashboard from "./dashboard";
 
 const Layout = () => {
   return (
     <div>
-      Layout page
-      <div>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
         <Outlet />
       </div>
     </div>
