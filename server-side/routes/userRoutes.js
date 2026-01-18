a1a69ecae3e2d24aafab246b272c46c7fa2e5c56
itsnehakv <itsnehakv@gmail.com>
2026-01-18 23:06:53 +0530

    Corrected '/login'  from '/path'

--- server-side/routes/userRoutes.js
+++ server-side/routes/userRoutes.js
@@ -10,7 +10,7 @@ import protect from "../middlewares/authMiddleware.js";
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
-userRouter.post("/path", loginUser);
+userRouter.post("/login", loginUser);
 userRouter.get("/data", protect, getUserById);
 userRouter.get("/resumes", protect, getUserResumes);
 
