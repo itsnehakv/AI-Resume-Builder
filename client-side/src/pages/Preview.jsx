d84fa00292429c48a3a762fbf57d26cd493c237c
itsnehakv <itsnehakv@gmail.com>
2026-01-21 19:36:42 +0530

    Added API, Fixed: Variable mismatch regarding accent_color

--- client-side/src/pages/Preview.jsx
+++ client-side/src/pages/Preview.jsx
@@ -1,35 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { dummyResumeData } from "../assets/assets";
 import ResumePreview from "../Components/ResumePreview";
 import Loader from "../Components/Loader";
 import { ArrowLeftIcon } from "lucide-react";
+import api from "../configs/api.js";
 
 //This page is displayed when
 const Preview = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
   const { resumeId } = useParams();
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const [resumeData, setResumeData] = useState(null);
 
   const loadResume = async () => {
-    setResumeData(
-      dummyResumeData.find((resume) => resume._id === resumeId || null)
-    );
-    setIsLoading(false);
+    try {
+      const { data } = await api.get(`/api/resumes/public/${resumeId}`);
+      setResumeData(data.resume);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
     loadResume();
   }, []);
+
   return resumeData ? (
     <div className="bg-slate-400">
       <div className="max-w-3xl mx-auto py-3">
         <ResumePreview
           data={resumeData}
           template={resumeData.template}
-          accentColor={resumeData.accentColor}
+          accentColor={resumeData.accent_color}
           classes="py-4 bg-white"
         />
       </div>
